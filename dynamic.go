a85aa6d06a6168082bff3ab0dfd9531d0f3e505a
jfrog-solutiontest <jfrog-solutions-ci@jfrog.com>
2020-08-02 17:24:57 -0700

    initial

---
+++ dynamic.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+    "fmt"
+    "github.com/jfrog-solutiontest/ci-basic-dep1"
+)
+
+func main () {
+    fmt.Println ("Package Name: akita")
+    ci_basic_dep1.PackageName()
+}
+
